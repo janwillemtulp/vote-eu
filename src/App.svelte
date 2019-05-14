52177cf79d3b19dc9074f792367eb303aa3d283e
Jan Willem Tulp <janwillemtulp@tulpinteractive.com>
2019-05-14 13:59:34 +0200

    extracted footer

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
   import Modal from "./Modal.svelte";
   import Header from "./Header.svelte";
   import Main from "./Main.svelte";
+  import Footer from "./Footer.svelte";
 
   let promise = loadData();
 
@@ -46,34 +47,6 @@
   }
 </script>
 
-<style>
-  footer {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    background-color: #fff;
-    width: 100%;
-    display: grid;
-    grid-template-columns: auto;
-    justify-content: center;
-    align-items: center;
-  }
-
-  footer .inner {
-    width: 940px;
-    display: grid;
-    grid-template-columns: auto 200px;
-    justify-content: right;
-    align-items: center;
-    height: 30px;
-    text-align: right;
-  }
-
-  footer .inner p {
-    margin: 0;
-  }
-</style>
-
 {#await promise}
   <p>...loading data</p>
 {:then}
@@ -87,19 +60,4 @@
 
 <!-- <Modal /> -->
 
-<footer>
-  <div class="inner">
-    <p>
-      concept & design by
-      <a
-        href="http://tulpinteractive.com"
-        title="TULP interactive"
-        target="_blank">
-        TULP interactive
-      </a>
-    </p>
-    <a href="about.html" target="_blank" title="about 2019 vote EU">
-      about 2019 vote EU
-    </a>
-  </div>
-</footer>
+<Footer />
---
+++ src/Footer.svelte
@@ -0,0 +1,44 @@
+<style>
+  footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    background-color: #fff;
+    width: 100%;
+    display: grid;
+    grid-template-columns: auto;
+    justify-content: center;
+    align-items: center;
+  }
+
+  footer .inner {
+    width: 940px;
+    display: grid;
+    grid-template-columns: auto 200px;
+    justify-content: right;
+    align-items: center;
+    height: 30px;
+    text-align: right;
+  }
+
+  footer .inner p {
+    margin: 0;
+  }
+</style>
+
+<footer>
+  <div class="inner">
+    <p>
+      concept & design by
+      <a
+        href="http://tulpinteractive.com"
+        title="TULP interactive"
+        target="_blank">
+        TULP interactive
+      </a>
+    </p>
+    <a href="about.html" target="_blank" title="about 2019 vote EU">
+      about 2019 vote EU
+    </a>
+  </div>
+</footer>
