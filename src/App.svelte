7bcc8342d15fe6f032996f54db3e555a286c4d87
Jan Willem Tulp <janwillemtulp@tulpinteractive.com>
2019-05-15 08:52:30 +0200

    generated country data + removed obsolete fields + extracted load data function

--- src/App.svelte
+++ src/App.svelte
@@ -1,63 +1,66 @@
 <script>
   import { csv } from "d3-fetch";
   import { data } from "./store.js";
+  import { loadData } from './common.js'
   import Modal from "./Modal.svelte";
   import Header from "./Header.svelte";
   import Main from "./Main.svelte";
   import Footer from "./Footer.svelte";
 
-  let promise = loadData();
+  let promise = loadData('UK')
+  console.log(promise)
+  // let promise = loadData();
 
-  const parse = attribute => (attribute === "" ? null : +attribute);
+  // const parse = attribute => (attribute === "" ? null : +attribute);
 
-  async function loadData() {
-    await csv("./data/data.csv", row => ({
-      ...row,
-      answer: parse(row.answer),
-      simplifiedAnswer:
-        parse(row.answer) === null
-          ? null
-          : parse(row.answer) < 50
-          ? 0
-          : parse(row.answer) > 50
-          ? 100
-          : 50,
-      economic_liberalisation: parse(row.economic_liberalisation),
-      environmental_protection: parse(row.environmental_protection),
-      european_integration: parse(row.european_integration),
-      law_and_order: parse(row.law_and_order),
-      liberal_society: parse(row.liberal_society),
-      party_id: parse(row.party_id),
-      question_id: parse(row.question_id),
-      question_shared_id: parse(row.question_shared_id),
-      restrictive_financial_policy: parse(row.restrictive_financial_policy),
-      restrictive_immigration_policy: parse(row.restrictive_immigration_policy),
-      xy_lc: parse(row.xy_lc),
-      xy_lr: parse(row.xy_lr),
-      country_code: ["VL", "WA"].includes(row.country_code)
-        ? "BE"
-        : row.country_code,
-      country_name: ["VL", "WA"].includes(row.country_code)
-        ? "Belgium"
-        : row.country_name
-    })).then(res => {
-      $data = res;
-      console.log(res);
-    });
-  }
+  // async function loadData() {
+  //   await csv("./data/data.csv", row => ({
+  //     ...row,
+  //     answer: parse(row.answer),
+  //     simplifiedAnswer:
+  //       parse(row.answer) === null
+  //         ? null
+  //         : parse(row.answer) < 50
+  //         ? 0
+  //         : parse(row.answer) > 50
+  //         ? 100
+  //         : 50,
+  //     economic_liberalisation: parse(row.economic_liberalisation),
+  //     environmental_protection: parse(row.environmental_protection),
+  //     european_integration: parse(row.european_integration),
+  //     law_and_order: parse(row.law_and_order),
+  //     liberal_society: parse(row.liberal_society),
+  //     party_id: parse(row.party_id),
+  //     question_id: parse(row.question_id),
+  //     question_shared_id: parse(row.question_shared_id),
+  //     restrictive_financial_policy: parse(row.restrictive_financial_policy),
+  //     restrictive_immigration_policy: parse(row.restrictive_immigration_policy),
+  //     xy_lc: parse(row.xy_lc),
+  //     xy_lr: parse(row.xy_lr),
+  //     country_code: ["VL", "WA"].includes(row.country_code)
+  //       ? "BE"
+  //       : row.country_code,
+  //     country_name: ["VL", "WA"].includes(row.country_code)
+  //       ? "Belgium"
+  //       : row.country_name
+  //   })).then(res => {
+  //     $data = res;
+  //     console.log(res);
+  //   });
+  // }
 </script>
 
 {#await promise}
   <p>...loading data</p>
 {:then}
-{:catch error}
-  <p style="color: red;">{error.message}</p>
-{/await}
-
 <Main />
 
 <Header />
 
-<!-- <Modal /> -->
+{:catch error}
+  <p style="color: red;">{error.message}</p>
+{/await}
 
+<!-- <Modal /> -->
 <Footer />
+
---
+++ src/common.js
@@ -0,0 +1,31 @@
+import { csv } from 'd3-fetch'
+import { data } from './store.js'
+
+const parse = attribute => (attribute === '' ? null : +attribute)
+
+export async function loadData(countryCode) {
+  csv(`./data/${countryCode}.csv`, row => ({
+    ...row,
+    answer: parse(row.answer),
+    simplifiedAnswer:
+      parse(row.answer) === null
+        ? null
+        : parse(row.answer) < 50
+          ? 0
+          : parse(row.answer) > 50
+            ? 100
+            : 50,
+    party_id: parse(row.party_id),
+    question_id: parse(row.question_id),
+    question_shared_id: parse(row.question_shared_id),
+    country_code: ['VL', 'WA'].includes(row.country_code)
+      ? 'BE'
+      : row.country_code,
+    country_name: ['VL', 'WA'].includes(row.country_code)
+      ? 'Belgium'
+      : row.country_name
+  })).then(res => {
+    data.set(res)
+    console.log(res)
+  })
+}
